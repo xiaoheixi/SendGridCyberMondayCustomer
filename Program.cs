8c6721315009fedd7f00f7acda318932c8de27c4
xiaoheixi <justinzhao893@gmail.com>
2022-11-16 15:13:33 +1100

    First Commit! Working without Template!

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// using SendGrid's C# Library
+// https://github.com/sendgrid/sendgrid-csharp
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    internal class Example
+    {
+        private static void Main()
+        {
+            Execute().Wait();
+        }
+
+        static async Task Execute()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("CyberMondayCustomerTest");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("confirmation@yqme.com.au", "YQme");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress("justin.zhao@techsol.net.au", "Justin");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+            Console.WriteLine("Email sent!");
+        }
+    }
+}
