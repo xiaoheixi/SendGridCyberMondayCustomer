8da63e4302cdc12937f6bf37b495a7ea5de18464
xiaoheixi <justinzhao893@gmail.com>
2022-11-17 11:14:09 +1100

    Working template integration.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,11 @@
 using Microsoft.Data.SqlClient;
 using SendGrid;
 using SendGrid.Helpers.Mail;
+var apiKey = Environment.GetEnvironmentVariable("CyberMondayCustomerTest");
+var client = new SendGridClient(apiKey);
+var from = new EmailAddress("confirmation@yqme.com.au", "YQme");
+var to = new EmailAddress("justin.zhao@techsol.net.au", "Justin");
+var templateId = "d-bed201f8dccf4f4ca2bcd20bbc2a9215";
 Console.WriteLine("Getting Connection ...");
 var datasource = @"yqme-prod-syd.database.windows.net"; //Server
 var database = "CafeOrder"; //Database Name
@@ -27,33 +32,47 @@ try {
     conn.Close();
     foreach (string x in arlist) {
         Console.WriteLine(x);
+        Console.WriteLine(x.Split(',')[2].TrimStart());
+        var dynamicTemplateData = new
+        {
+            subject = "Cyber Monday Specials!",
+            recipientName = x.Split(',')[2].TrimStart(), 
+            WebsiteNumber = x.Split(',')[1].TrimStart(),
+            PhoneNumber = x.Split(',')[4].TrimStart()
+        };
+        var msg = MailHelper.CreateSingleTemplateEmail(from, to, templateId, dynamicTemplateData);
+        var response = await client.SendEmailAsync(msg);
+        if (response.IsSuccessStatusCode)
+        {
+            Console.WriteLine("Email has been sent successfully");
+        }
     }
 }
 catch (Exception e) {
     Console.WriteLine("Error: " + e.Message);
 }
-var apiKey = Environment.GetEnvironmentVariable("CyberMondayCustomerTest");
-var client = new SendGridClient(apiKey);
-var from = new EmailAddress("confirmation@yqme.com.au", "YQme");
-var to = new EmailAddress("justin.zhao@techsol.net.au", "Justin");
-var templateId = "d-bed201f8dccf4f4ca2bcd20bbc2a9215";
-var dynamicTemplateData = new
-{
-    subject = "Cyber Monday Specials!",
-    recipientName = "Insert PatronName", 
-    todoItemList = new[]
-    {
-        new { title = "Organize invoices", dueDate = "11 June 2022", status = "Completed" },
-        new { title = "Prepare taxes", dueDate = "12 June 2022", status = "In progress" },
-        new { title = "Submit taxes", dueDate = "25 June 2022", status = "Pending" },
-    }
-};
-var msg = MailHelper.CreateSingleTemplateEmail(from, to, templateId, dynamicTemplateData);
-var response = await client.SendEmailAsync(msg);
-if (response.IsSuccessStatusCode)
-{
-    Console.WriteLine("Email has been sent successfully");
-}
+// var apiKey = Environment.GetEnvironmentVariable("CyberMondayCustomerTest");
+// var client = new SendGridClient(apiKey);
+// var from = new EmailAddress("confirmation@yqme.com.au", "YQme");
+// var to = new EmailAddress("justin.zhao@techsol.net.au", "Justin");
+// var templateId = "d-bed201f8dccf4f4ca2bcd20bbc2a9215";
+// var dynamicTemplateData = new
+// {
+//     subject = "Cyber Monday Specials!",
+//     // recipientName = arlist(0).Split(',')[2].TrimStart(), 
+//     todoItemList = new[]
+//     {
+//         new { title = "Organize invoices", dueDate = "11 June 2022", status = "Completed" },
+//         new { title = "Prepare taxes", dueDate = "12 June 2022", status = "In progress" },
+//         new { title = "Submit taxes", dueDate = "25 June 2022", status = "Pending" },
+//     }
+// };
+// var msg = MailHelper.CreateSingleTemplateEmail(from, to, templateId, dynamicTemplateData);
+// var response = await client.SendEmailAsync(msg);
+// if (response.IsSuccessStatusCode)
+// {
+//     Console.WriteLine("Email has been sent successfully");
+// }
 /*
     References
     https://www.twilio.com/blog/send-emails-with-csharp-handlebars-templating-and-dynamic-email-templates
